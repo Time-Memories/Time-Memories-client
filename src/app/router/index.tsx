1cb93e2940c9016dff6e3c22a87425dc2c6cbbdf
hoon-ground <break_the_limit@naver.com>
2026-06-09 10:41:30 +0900

    [#5]Feat: 방화면 라우터 연결

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -3,12 +3,14 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { MainLayout } from '@widgets/main-layout';
 import { HomePage } from '@pages/home';
 import { NotFoundPage } from '@pages/not-found';
+import { RoomPage } from '@pages/room';
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="rooms/:roomId" element={<RoomPage />} />
       </Route>
       <Route path="/404" element={<NotFoundPage />} />
       <Route path="*" element={<Navigate to="/404" replace />} />
